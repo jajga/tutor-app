9e0a998b7cc8c0749916c79e76706a9035377df8
System Administrator <root@Narendras-MacBook-Pro.local>
2019-08-04 21:41:16 +0530

    Additional Files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './home/Home';
+import Nav from './navigation/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,24 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
+          
         </a>
+
+        <a
+          className="App-link"
+          href="/about"
+        >
+          About
+
+          
+        </a>
+
+        <Router>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={Home} />
+            <Route path="/topics" component={Home} />
+          </Router>  
       </header>
     </div>
   );
